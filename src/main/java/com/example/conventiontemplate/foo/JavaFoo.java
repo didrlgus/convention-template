f92e1bb99cc0906033583a2584f4a009b0b9f780
didrlgus <rlgusdid@naver.com>
2021-09-04 17:01:24 +0900

    feat: minor version up test

--- src/main/java/com/example/conventiontemplate/foo/JavaFoo.java
+++ src/main/java/com/example/conventiontemplate/foo/JavaFoo.java
@@ -5,7 +5,7 @@ public class JavaFoo {
     public String callMe() {
         System.out.println("Please, call me");
         System.out.println("새로운 기능 추가");
-
+        System.out.println("새로운 기능 추가2");
         return "hello world";
     }
 
