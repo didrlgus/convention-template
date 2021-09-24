8fa411e7e8945484e8dc3a758301acdf269a0892
didrlgus <rlgusdid@naver.com>
2021-09-24 18:53:07 +0900

    feat: 환경변수 테스트

--- script/prepare-release.sh
+++ script/prepare-release.sh
@@ -5,6 +5,9 @@ echo "version = $1"
 # Get version number from version tag
 JAR_VERSION=`echo $1 | cut -d'v' -f2`
 echo "jar = $JAR_VERSION"
+echo "### MYSQL_DATABASE_NAME: $MYSQL_DATABASE_NAME"
+echo "### LOCAL_MYSQL_USERNAME: $LOCAL_MYSQL_USERNAME"
+echo "### LOCAL_MYSQL_PASSWORD: $LOCAL_MYSQL_PASSWORD"
 
 # Package the new library version and copy it to release folder
 # These files will be upload to github by @semantic-release/github
